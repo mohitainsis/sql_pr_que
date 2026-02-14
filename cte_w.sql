8e4e3f2cb11a9f01af3f477879ae23e9b25de06d
Mohit <thisismohitstm@gmail.com>
2026-02-14 15:03:25 +0530

    ctes

--- cte_w.sql
+++ cte_w.sql
@@ -23,3 +23,63 @@ select name,salary
 from employees1 e,avg_sal as avs
 where e.salary>avs.average_salary;
 
+
+-------------
+
+with dept_avg as(
+select dept,cast(avg(salary)as int) avg_salary
+from employees1
+group by dept
+)
+select *
+from dept_avg
+where avg_salary>50000;
+
+-------------
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+INSERT INTO orders (order_id, customer_id, amount) VALUES
+(1, 101, 500),
+(2, 102, 800),
+(3, 101, 700),
+(4, 103, 300);
+SELECT * FROM orders;
+
+-----------
+with c_g as(
+select customer_id , sum(amount) as total_amount
+from orders
+group by customer_id
+)
+select *
+from c_g
+where total_amount>1000;
+
+------------
+
+select * from employees1;
+
+with cte as(
+select dept,max(salary) as max_salary
+from employees1
+group by dept
+)
+select name,salary
+from employees1 e
+join cte c on e.dept = c.dept
+and e.salary=c.max_salary
+order by salary desc;
+
+with cte as(
+select dept,cast(avg(salary)as int) as avg_sal
+from employees1
+group by dept
+)
+select e.name,e.salary,e.dept
+from employees1 e
+join cte c on c.dept=e.dept
+where c.avg_sal <e.salary;
